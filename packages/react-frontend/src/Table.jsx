6dc8a1a811f7217aa7c039025a98a0db46e0eee1
Konrad <>
2025-04-08 19:20:09 -0700

    step 6 commit

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -10,9 +10,25 @@ function TableHeader() {
 }
 
 function TableBody(props) {
-  return (
-    <tbody />
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+   }
   );
+  return (
+      <tbody>
+        {rows}
+       </tbody>
+   );
 }
 
 function Table(props) {
