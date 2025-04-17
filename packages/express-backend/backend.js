21a8a45bae11860419a3e857115146dec51843ae
Konrad <>
2025-04-17 12:02:29 -0700

    almost done with a2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,9 +40,57 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+//const deleteUser = (user) => {
+//  users["users_list"].
+
 app.get("/users", (req, res) => {
-  res.send(users);
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+/*
+app.delete("/users", (req, res) => {
+  const userToDelete = req.body;
+  deleteUser(userToDelete);
+  res.send();
+});
+*/
 
 app.listen(port, () => {
   console.log(
